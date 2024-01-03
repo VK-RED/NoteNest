f11c6a855f7bcdb1a545b2aae3866a3bd30bc626
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-03 14:00:05 +0530

    add body parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { Request,Response } from 'express';
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!')
