86893cab02c4ff208bee90b3109ad4df66e7c6d6
Vishnu Kumar <vishnukumart96@gmail.com>
2024-01-03 23:01:12 +0530

    add crud routes for notes

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -3,7 +3,7 @@ import {prisma} from "../index"
 import {z} from 'zod';
 import { EditNotesProps, createNotesProps } from '../types';
 import { authMiddleware } from '../middlewares/auth';
-import { NEW_NOTE_ADDED, NOTE_EDITED, NO_NOTE_FOUND, SIGN_AGAIN, UNFORSEEN_ERROR } from '../constants';
+import { NEW_NOTE_ADDED, NOTE_DELETED, NOTE_EDITED, NO_NOTE_FOUND, SIGN_AGAIN, UNFORSEEN_ERROR } from '../constants';
 
 const {Router} = express;
 
@@ -24,6 +24,8 @@ notesRouter.post("/", authMiddleware, async (req:Request,res:Response) => {
         });
 
         if(!user) return res.json({message:SIGN_AGAIN});
+
+        //create a new note
 
         const newNote = await prisma.personalNotes.create({
             data:{
@@ -63,6 +65,8 @@ notesRouter.put("/:id", authMiddleware, async (req : Request,res : Response) => 
 
         if(!note) return res.json({message:NO_NOTE_FOUND});
 
+        //update the new metadata
+
         if(parsedData.title){
             await prisma.personalNotes.update({
                 where:{
@@ -96,3 +100,98 @@ notesRouter.put("/:id", authMiddleware, async (req : Request,res : Response) => 
         return res.json({message:UNFORSEEN_ERROR});
     }
 })
+
+// delete a note
+
+notesRouter.delete("/:id", authMiddleware, async (req:Request,res:Response) => {
+
+    const noteId = req.params['id'];
+    
+    try {
+        
+        const note = await prisma.personalNotes.findUnique({
+            where:{
+                id: noteId,
+            }
+        })
+
+        if(!note) return res.json({message:NO_NOTE_FOUND});
+
+        //delete the note
+
+        await prisma.personalNotes.delete({
+            where:{
+                id: noteId,
+            }
+        })
+
+        return res.json({message:NOTE_DELETED})
+
+
+    } catch (error) {
+        if(error instanceof z.ZodError){
+            console.log(error.errors);
+            return res.json({error:error.errors});
+        }
+
+        return res.json({message:UNFORSEEN_ERROR});
+    }
+
+
+})
+
+// get a single note
+
+notesRouter.get("/:id", authMiddleware, async (req:Request,res:Response) => {
+
+    const noteId = req.params['id'];
+    
+    try {
+        
+        const note = await prisma.personalNotes.findUnique({
+            where:{
+                id: noteId,
+            },
+        })
+
+        if(!note) return res.json({message:NO_NOTE_FOUND});
+        return res.json({note})
+
+    } catch (error) {
+
+        if(error instanceof z.ZodError){
+            console.log(error.errors);
+            return res.json({error:error.errors});
+        }
+
+        return res.json({message:UNFORSEEN_ERROR});
+    }
+
+})
+
+// get all notes
+
+notesRouter.get("/", authMiddleware, async (req:Request,res:Response) => {
+
+    const userId = req.headers['userId'] as string;
+    
+    try {
+        
+        const allNotes = await prisma.personalNotes.findMany({
+            where:{
+                userId,
+            }
+        })
+        
+        return res.json({allNotes})
+
+    } catch (error) {
+
+        console.log(error);
+        return res.json({message:UNFORSEEN_ERROR});
+    }
+
+})
+
+
+
